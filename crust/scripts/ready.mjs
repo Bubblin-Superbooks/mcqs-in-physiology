9aeeb5236e668408c747af8b9d6a15fb61417601
marvindanig <marvin@bubbl.in>
2023-01-20 15:36:46 -0500

    Make this repo mjs/imports compatible.

--- crust/scripts/ready.mjs
+++ crust/scripts/ready.mjs
@@ -1,10 +1,9 @@
-(function (r) {
   'use strong'
 
-  const fse = r('fs-extra')
-  const chalk = r('chalk')
+  import fs from 'fs-extra'
+  import chalk from 'chalk'
 
-  fse.readFile('./license.txt', 'utf8', (err, data) => {
+  fs.readFile('./license.txt', 'utf8', (err, data) => {
     if (err) {
       return new Error('Couldn\'t read license information.')
     }
@@ -12,4 +11,3 @@
   })
 
   console.log(chalk.blue('Project is ready.'))
-})(require)
